17a508388fd1f5b34c9896c6d41b7897cd2efb7f
Daniel <db_101@icloud.com>
2020-08-13 15:33:26 -0700

    update

--- src/test/java/definitions/JavaStepDefs.java
+++ src/test/java/definitions/JavaStepDefs.java
@@ -61,4 +61,43 @@ public class JavaStepDefs {
             System.out.println("This url is missing");
         }
     }
+
+    @And("I print if number {int} is positive")
+    public void iPrintIfNumberIsPositive(int num) {
+        if (num >= 0) {
+            System.out.println("Number is positive: " + num);
+        }
+        else {
+            System.out.println("Number is negative: " + num);
+        }
+    }
+
+    @And("I print {int} th day of week")
+    public void iPrintThDayOfWeek(int day) {
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("We use Earth days");
+        }
+    }
 }
--- src/test/resources/features/java.feature
+++ src/test/resources/features/java.feature
@@ -7,3 +7,5 @@ Feature: Java feature
     And I perform actions with "var1" and "VAR1"
     And I do homework for day 5
     And I print url for "google" page
+    And I print if number 35 is positive
+    And I print 7 th day of week
